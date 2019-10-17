a782b948760dbdccbb26fc0ec105fdf95700dc99
John Speck <speckworks@gmail.com>
2019-10-17 08:20:54 -0400

    postgres changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_15_203854) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "aquaria", force: :cascade do |t|
     t.integer "size"
     t.boolean "water"
